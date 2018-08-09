06670acb3eda699c2481e5c58a2bb12031996516
kazinur <kazinur@mybihs.org>
2018-08-09 15:29:37 +0000

    automatically backed up by learn

--- index.rb
+++ index.rb
@@ -43,7 +43,7 @@ def city_populations(cities)
   puts cities [:new_york][:population]
   puts cities [:miami][:website]
   puts cities [:los_angeles][:mayor]
-  
+  puts chicago_hash= {}
 end
 city_populations(cities)
 
