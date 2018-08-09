1df2c77ac9714440eacf1cdab02c9a84d99c3d3f
kazinur <kazinur@mybihs.org>
2018-08-09 15:50:59 +0000

    automatically backed up by learn

--- index.rb
+++ index.rb
@@ -7,10 +7,11 @@ ingredients = [
 ]
 
 def sandwich_request(ingredients, order) 
-  puts sandwich_request "ingredients #[3][0], [3][1], [3][2]"  
-  puts sandwich_request "ingredients #[2][2]}"
-  puts sandwich_request "ingredients #[0][1]}"
+  puts sandwich_request "ingredients #{[3][0], [3][1], [3][2]}"  
+  puts sandwich_request "ingredients #{ [2][2]}"
+  puts sandwich_request "ingredients #{ [0][1]}"
 end
+end  
 
 # example city_info hash
 cities = {
