378a38640c5f1599283c5a488fc84148f41a35f6
kazinur <kazinur@mybihs.org>
2018-08-09 14:49:00 +0000

    automatically backed up by learn

--- index.rb
+++ index.rb
@@ -1,39 +1,39 @@
-# example ingredients array
-ingredients = [
-    ["rye", "sourdough", "baguette"],
-    ["ham", "salami", "turkey"],
-    ["swiss", "munster", "cheddar"],
-    ["mayo", "mustard", "tabasco"]
-]
+# # example ingredients array
+# ingredients = [
+#     ["rye", "sourdough", "baguette"],
+#     ["ham", "salami", "turkey"],
+#     ["swiss", "munster", "cheddar"],
+#     ["mayo", "mustard", "tabasco"]
+# ]
 
-def sandwich_request(ingredients, order) 
-  puts "ingredients #{ingredients[3][0], ingredients[3][1], ingredients[3][2]}."  
-end
+# def sandwich_request(ingredients, order) 
+#   puts "ingredients #{ingredients[3][0], ingredients[3][1], ingredients[3][2]}."  
+# end
 
-# example city_info hash
-cities = {
-  :new_york => { 
-    :mayor => "Bill DeBlasio",
-    :population => 8337000,
-    :website => "http://www.nyc.gov"
-  },
-  :los_angeles => { 
-    :mayor => "Eric Garcetti",
-    :population => 3884307,
-    :website => "http://www.lacity.org"
-  },
-  :miami => { 
-    :mayor => "Tomás Regalado",
-    :population => 419777,
-    :website => "http://www.miamigov.com"
-  },
-  :chicago => { 
-    :mayor => "Rahm Emanuel",
-    :population => 2695598,
-    :website => "http://www.cityofchicago.org/"
-  }
-}
+# # example city_info hash
+# cities = {
+#   :new_york => { 
+#     :mayor => "Bill DeBlasio",
+#     :population => 8337000,
+#     :website => "http://www.nyc.gov"
+#   },
+#   :los_angeles => { 
+#     :mayor => "Eric Garcetti",
+#     :population => 3884307,
+#     :website => "http://www.lacity.org"
+#   },
+#   :miami => { 
+#     :mayor => "Tomás Regalado",
+#     :population => 419777,
+#     :website => "http://www.miamigov.com"
+#   },
+#   :chicago => { 
+#     :mayor => "Rahm Emanuel",
+#     :population => 2695598,
+#     :website => "http://www.cityofchicago.org/"
+#   }
+# }
 
-def city_populations(cities)
-  puts   
-end
+# def city_populations(cities)
+#   puts   
+# end
